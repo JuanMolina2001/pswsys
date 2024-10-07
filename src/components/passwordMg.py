3edddfebf906dc452cf008d406b05b2a0c634e6d
JuanMolina2001 <juanmolinajm2001@gmail.com>
2024-10-07 03:16:31 -0300

    add manage for passwords (copy,delete)

--- src/components/passwordMg.py
+++ src/components/passwordMg.py
@@ -1,23 +1,68 @@
-from tkinter import Toplevel, Label, Entry, Button, messagebox
-from ..utils import auth
+from tkinter import Toplevel, Label, Entry, Button, messagebox, ttk, Frame, Tk
+from ..utils import auth, dataEncrypt
+import json
+
 class PasswordMg:
-    def copy(self):
-        Label(self.modal, text="Write the user password").pack()
-        password = Entry(self.modal, show="*")
-        password.pack()
-        password.bind("<Return>", lambda event: self.verify(password))
-    def verify(self,password):
-        if auth(password.get()):
-            value = self.data["values"][-1]
-            self.window.clipboard_clear()
-            self.window.clipboard_append(value)
-            self.modal.destroy()
-        else:
-            messagebox.showerror("Error", "Invalid password")
-    def __init__(self ,data,window):
-        self.data = data
+    def __init__(
+        self, selected, treeView: ttk.Treeview, window: Tk, oldContainer: Frame,key
+    ):
+        self.key = key
+        self.selected = selected
+        self.treeView = treeView
         self.window = window
-        self.modal = Toplevel(self.window)
-        self.modal.title("User Password")
-        self.modal.resizable(False, False)
-        Button(self.modal, text="copy", command=self.copy).pack()
+        self.container = Frame(window)
+        self.oldContainer = oldContainer
+        columns = ("Source", "User/Email", "Password", "hidden")
+        list_box = ttk.Treeview(
+            self.container, columns=columns, show="headings", height=1
+        )
+        for col in columns:
+            if col == "hidden":
+                list_box.column(col, width=0, stretch=False)
+                continue
+            list_box.heading(col, text=col.title(), anchor="center")
+            list_box.column(col, anchor="center", width=150)
+        list_box.insert("", "end", values=treeView.item(selected)["values"])
+        list_box.pack()
+        Button(
+            self.container, text="copy", command=lambda: self.verify(self.copy)
+        ).pack()
+        Button(
+            self.container, text="delete", command=lambda: self.verify(self.delete)
+        ).pack()
+        self.container.pack()
+
+    def copy(self):
+        value = self.treeView.item(self.selected)["values"][-1]
+        self.window.clipboard_clear()
+        self.window.clipboard_append(value)
+
+    def delete(self):
+        self.treeView.delete(self.selected)
+        self.container.forget()
+        self.oldContainer.pack()
+        data = []
+        columns = ["source", "user", "d", "password"]
+
+        for row in self.treeView.get_children():
+            dictionary = dict(zip(columns, self.treeView.item(row)["values"]))
+            del dictionary["d"]
+            data.append(dictionary)
+        print(json.dumps(data))
+        dataEncrypt().write(json.dumps(data).encode(), self.key)
+    
+    def verify(self, callback):
+        modal = Toplevel(self.window)
+        modal.title("Password")
+        Label(modal, text="Write the user password of windows").pack()
+        password = Entry(modal, show="*")
+        password.pack()
+
+        def onReturn(_):
+            if auth(password.get()):
+                modal.destroy()
+                callback()
+            else:
+                messagebox.showerror("Error", "Invalid password")
+
+        password.bind("<Return>", onReturn)
--- src/components/pswList.py
+++ src/components/pswList.py
@@ -1,10 +1,11 @@
-from tkinter import ttk,Frame,Tk,Label,Entry,Button,Toplevel,messagebox
+from tkinter import ttk,Frame,Tk
 from ..utils import auth
 from .passwordMg import PasswordMg
 class PswList:
-    def __init__(self, listPasswords: list, window):
+    def __init__(self, listPasswords: list, window,key):
         self.window:Tk = window
         self.container = Frame(window)
+        self.listPasswords = listPasswords
         columns = ("Source","User/Email", "Password", "hidden")
         self.list_box = ttk.Treeview(self.container, columns=columns, show='headings')
         for col in columns:
@@ -14,15 +15,19 @@ class PswList:
             self.list_box.heading(col, text=col.title(), anchor="center")
             self.list_box.column(col, anchor="center", width=150)
         for password in listPasswords:
-            source = password['source'] or ""
-            user = password['user'] or ""
-            value = password['password']
-            psw = '*' * len(password['password'])
+            source = str(password['source']) or ""
+            user = str(password['user']) or ""
+            value = str(password['password'])
+            psw = '*' * len(str(password['password']))
             self.list_box.insert('', 'end', values=(source,user, psw,value)) 
 
         scrollbar = ttk.Scrollbar(self.container, orient="vertical", command=self.list_box.yview)
         self.list_box.configure(yscroll=scrollbar.set)
-        self.list_box.bind("<<TreeviewSelect>>", lambda event :PasswordMg(self.window,self.list_box.item(self.list_box.selection()[0])))
+        def onSelection(_):
+            if self.list_box.selection():
+                self.container.forget()
+                PasswordMg(self.list_box.selection(),self.list_box,window,self.container,key)
+        self.list_box.bind("<<TreeviewSelect>>", onSelection)
         self.container.pack()
         self.list_box.pack()
 
