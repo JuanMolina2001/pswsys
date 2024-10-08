1d996e8e170ba57e6d773c3c1e3d6f46c688deaf
JuanMolina2001 <juanmolinajm2001@gmail.com>
2024-10-08 03:15:54 -0300

    Refactor build.bat to include copying logo.ico file

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 pyinstaller --noconsole --name pswsys --icon=logo.ico main.py
 xcopy src\assets dist\pswsys\src\assets /E /I
 copy src\settings.json dist\pswsys\src\settings.json
+copy logo.ico dist\pswsys\logo.ico
