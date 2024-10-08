6f2e1fac59e3bceda017e1159dc65b6f5876f9da
JuanMolina2001 <juanmolinajm2001@gmail.com>
2024-10-07 22:54:48 -0300

    Refactor PasswordMg class

--- src/components/passwordMg.py
+++ src/components/passwordMg.py
@@ -1,10 +1,16 @@
-from tkinter import Toplevel, Label, Entry, Button, messagebox, ttk, Frame, Tk
-from ..utils import auth, dataEncrypt
+from tkinter import Label, Entry, Button, messagebox, ttk, Frame, Tk
+from ..utils import dataEncrypt, verify
 import json
+
 
 class PasswordMg:
     def __init__(
-        self, selected, treeView: ttk.Treeview, window: Tk, oldContainer: Frame,key
+        self,
+        selected=None,
+        treeView: ttk.Treeview = None,
+        window: Tk = None,
+        oldContainer: Frame = None,
+        key=None,
     ):
         self.key = key
         self.selected = selected
@@ -12,6 +18,14 @@ class PasswordMg:
         self.window = window
         self.container = Frame(window)
         self.oldContainer = oldContainer
+        self.container.pack()
+        oldContainer.forget()
+        if not selected:
+            self.create()
+        else:
+            self.update()
+
+    def update(self):
         columns = ("Source", "User/Email", "Password", "hidden")
         list_box = ttk.Treeview(
             self.container, columns=columns, show="headings", height=1
@@ -22,25 +36,44 @@ class PasswordMg:
                 continue
             list_box.heading(col, text=col.title(), anchor="center")
             list_box.column(col, anchor="center", width=150)
-        list_box.insert("", "end", values=treeView.item(selected)["values"])
+        list_box.insert("", "end", values=self.treeView.item(self.selected)["values"])
         list_box.pack()
+        buttonFrame = Frame(self.container)
+        buttonFrame.pack()
+        Button(buttonFrame, text="Back", command=self.back).grid(
+            row=0, column=0, padx=10, pady=10
+        )
+        Button(buttonFrame, text="copy", command=self.copy).grid(
+            row=0, column=1, padx=10, pady=10
+        )
+        Button(buttonFrame, text="delete", command=self.delete).grid(
+            row=0, column=2, padx=10, pady=10
+        )
+    def create(self):
+        self.source = Entry(self.container)
+        self.user = Entry(self.container)
+        self.password = Entry(self.container, show="*")
+        Label(self.container, text="Source").pack()
+        self.source.pack()
+        Label(self.container, text="User/Email").pack()
+        self.user.pack()
+        Label(self.container, text="Password").pack()
+        self.password.pack()
         Button(
-            self.container, text="copy", command=lambda: self.verify(self.copy)
+            self.container,
+            text="Submit",
+            command=self.add,
         ).pack()
-        Button(
-            self.container, text="delete", command=lambda: self.verify(self.delete)
-        ).pack()
-        self.container.pack()
-
+    @verify
     def copy(self):
         value = self.treeView.item(self.selected)["values"][-1]
         self.window.clipboard_clear()
         self.window.clipboard_append(value)
 
+    @verify
     def delete(self):
         self.treeView.delete(self.selected)
-        self.container.forget()
-        self.oldContainer.pack()
+
         data = []
         columns = ["source", "user", "d", "password"]
 
@@ -48,21 +81,32 @@ class PasswordMg:
             dictionary = dict(zip(columns, self.treeView.item(row)["values"]))
             del dictionary["d"]
             data.append(dictionary)
-        print(json.dumps(data))
         dataEncrypt().write(json.dumps(data).encode(), self.key)
-    
-    def verify(self, callback):
-        modal = Toplevel(self.window)
-        modal.title("Password")
-        Label(modal, text="Write the user password of windows").pack()
-        password = Entry(modal, show="*")
-        password.pack()
+        self.container.destroy()
+        self.oldContainer.pack()
 
-        def onReturn(_):
-            if auth(password.get()):
-                modal.destroy()
-                callback()
-            else:
-                messagebox.showerror("Error", "Invalid password")
+    def back(self):
+        self.container.destroy()
+        self.oldContainer.pack()
 
-        password.bind("<Return>", onReturn)
+    @verify
+    def add(self):
+        if self.password.get() == "":
+            messagebox.showerror("Error", "Password cannot be empty")
+        else:
+            decryptData = dataEncrypt().read(self.key)
+            data: list = json.loads(decryptData)
+            data.append(
+                {
+                    "source": self.source.get(),
+                    "user": self.user.get(),
+                    "password": self.password.get(),
+                }
+            )
+            dataEncrypt().write(json.dumps(data).encode(), self.key)
+            psw = '*' * len(str(self.password.get()))
+            self.treeView.insert(
+                "", "end", values=(self.source.get(), self.user.get(), psw, self.password.get())
+            )
+            self.container.destroy()
+            self.oldContainer.pack()
